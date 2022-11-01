2109d5d8e51852ec9d6e89519b28889f34547790
mattiasvdl <mattias.vandelannoote@gmail.com>
2022-11-01 19:50:35 +0100

    Added Script to Update User Data in Azure Active Directory

---
+++ Azure Active Directory/UpdateUserData.ps1
@@ -0,0 +1,14 @@
+﻿Connect-AzureAD
+
+$csvDataFile = Import-Csv -Delimiter ";" -Path "C:\Temp\AAD\ADInformationUpdate.csv"
+
+foreach($record in $csvDataFile){
+
+    Write-Output $record.UserPrincipalName
+
+    if ($record.c) {
+        Set-AzureADUser -ObjectId $record.UserPrincipalName -UsageLocation $record.c
+    }
+}
+
+Disconnect-AzureAD
